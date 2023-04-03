467ca007d0215b416cd7ae88a761c5631fbe97c4
belhadjkacemdonia <bhkdonia@gmail.com>
2023-04-03 14:25:47 +0100

    firebase

--- lib/acceuil.dart
+++ lib/acceuil.dart
@@ -9,7 +9,7 @@ import 'inscrir.dart';
 
 class acceuil extends StatelessWidget {
   final List<String> imageList = [
-    "https://i0.wp.com/lapresse.tn/wp-content/uploads/2021/08/20210814_153219.jpg?fit=649%2C387&ssl=1",
+
     "https://i0.wp.com/lapresse.tn/wp-content/uploads/2021/12/vaccin-covid3.jpg?fit=1200%2C675&ssl=1",
     "https://madrassatii.com/wp-content/uploads/2021/10/%D8%AC%D9%88%D8%A7%D8%B2-%D8%A7%D9%84%D8%AA%D9%84%D9%82%D9%8A%D8%AD-%D9%83%D9%88%D9%81%D9%8A%D8%AF-scaled.jpg",
 
